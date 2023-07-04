11fa4a9ae613fc1244f4336baee5d3b2f7ac9e74
AlcoRV <zennerqwert@ya.ru>
2023-07-04 15:41:59 +0300

    Added smtp options

---
+++ MDL/Options/SmtpServerOptions.cs
@@ -0,0 +1,30 @@
+﻿namespace MDL.Options
+{
+    /// <summary>
+    ///     Настройки SMTP-сервера
+    /// </summary>
+    public sealed class SmtpServerOptions
+    {
+        /// <summary>
+        ///     Название секции конфигурации
+        /// </summary>
+        public const string SmtpServer = "SmtpServer";
+
+        /// <summary>
+        ///     Отображаемое имя почтового ящика отправки
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        ///     Адрес сервера почтового сервиса
+        /// </summary>
+        public string Client { get; set; }
+        /// <summary>
+        ///     Почтовый адрес отправителя
+        /// </summary>
+        public string Address { get; set; }
+        /// <summary>
+        ///     Персональный ключ SMTP-сервера
+        /// </summary>
+        public string Password { get; set; }
+    }
+}
